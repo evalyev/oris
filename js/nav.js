050679f68d96c7d130c619e9f28de09e652e08fc
Евгений Бакаев <ebakaev@eclient24.ru>
2022-04-04 17:20:35 +0300

    feat: добавил перечень услуг

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,42 @@
 function nav() {
-  const navList = Array.from(document.querySelectorAll('.header__nav-link'));
-  const navCont = document.querySelector('.header__nav');
-  let computedStyle = getComputedStyle(navCont, null)
-  let paddings = parseInt(computedStyle.getPropertyValue('padding-left')) * 2;
-  paddings = parseInt(computedStyle.getPropertyValue('width')) - paddings;
+  function navPaddings() {
+    const navList = Array.from(document.querySelectorAll('.header__nav-link'));
+    const navCont = document.querySelector('.header__nav');
+    let computedStyle = getComputedStyle(navCont, null)
+    let paddings = parseInt(computedStyle.getPropertyValue('padding-left')) * 2;
+    paddings = parseInt(computedStyle.getPropertyValue('width')) - paddings;
 
-  navList.forEach(item => {
-    paddings -= item.clientWidth;
-  })
+    navList.forEach(item => {
+      paddings -= item.clientWidth;
+    })
 
-  paddings = paddings / (navList.length * 2 + 2)  
+    paddings = paddings / (navList.length * 2 + 2)
 
-  navList.forEach(item => {
-    item.style.paddingLeft = paddings + 'px';
-    item.style.paddingRight = paddings + 'px';
-  })
+    navList.forEach(item => {
+      item.style.paddingLeft = paddings + 'px';
+      item.style.paddingRight = paddings + 'px';
+    })
+  }
+
+  function services() {
+    const btnServices = document.querySelector('.header__nav-link_type_services');
+    btnServices.addEventListener('mouseover', () => {
+      const services = document.querySelector('.header__services');
+
+      function mouseOut(evt) {
+        if (!(evt.target.closest('.header__services') || evt.target.closest('.header__nav-link_type_services'))) {
+          services.classList.remove('header__services_active');
+          document.removeEventListener('click', mouseOut);
+        }
+
+      }
+      services.classList.add('header__services_active');
+      document.addEventListener('click', mouseOut)
+    })
+  }
+
+  navPaddings();
+  services();
 }
 
 nav();
