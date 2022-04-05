0cc8465caaec53b89349969afa37f658554ec2d5
Евгений Бакаев <ebakaev@eclient24.ru>
2022-04-05 23:41:00 +0300

    feat: добавил ссылки на услуги

--- js/services.js
+++ js/services.js
@@ -3,7 +3,26 @@ function services() {
   servicesList.forEach(item => {
     item.addEventListener('click', (evt) => {
       evt.target.closest('.services__element').querySelector('.services__list').classList.toggle('services__list_active');
-    })
+    });
+
+    if (item.id === document.location.hash.slice(1)) {
+      item.querySelector('.services__list').classList.add('services__list_active');
+    }
+  });
+
+
+  window.addEventListener('hashchange', (e) => {
+    servicesList.forEach(item => {
+      if (item.id === document.location.hash.slice(1)) {
+        item.querySelector('.services__list').classList.add('services__list_active');
+
+        servicesList.forEach(item2 => {
+          if (item2 !== item) {
+            item2.querySelector('.services__list').classList.remove('services__list_active')
+          }
+        })
+      }
+    });
   })
 }
 
