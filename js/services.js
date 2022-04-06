799b56262d31e6af9077ff1fe55c9c82b82dfb74
Евгений Бакаев <ebakaev@eclient24.ru>
2022-04-06 04:01:42 +0300

    feat: в услугах подкорректирован текст

--- js/services.js
+++ js/services.js
@@ -3,10 +3,12 @@ function services() {
   servicesList.forEach(item => {
     item.addEventListener('click', (evt) => {
       evt.target.closest('.services__element').querySelector('.services__list').classList.toggle('services__list_active');
+      evt.target.closest('.services__element').querySelector('.services__title').classList.toggle('services__title_active')
     });
 
     if (item.id === document.location.hash.slice(1)) {
       item.querySelector('.services__list').classList.add('services__list_active');
+      item.querySelector('.services__title').classList.add('services__title_active')
     }
   });
 
@@ -15,10 +17,12 @@ function services() {
     servicesList.forEach(item => {
       if (item.id === document.location.hash.slice(1)) {
         item.querySelector('.services__list').classList.add('services__list_active');
+        item.querySelector('.services__title').classList.add('services__title_active')
 
         servicesList.forEach(item2 => {
           if (item2 !== item) {
             item2.querySelector('.services__list').classList.remove('services__list_active')
+            item2.querySelector('.services__title').classList.remove('services__title_active')
           }
         })
       }
